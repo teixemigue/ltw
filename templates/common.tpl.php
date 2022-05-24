6207991812a953710bbc9dee0f556f6b9a548dc9
shini <raquelmcarneiro16@gmail.com>
2022-05-24 13:02:11 +0100

    updated common.css
    created css when the user is logged in and logged out

--- templates/common.tpl.php
+++ templates/common.tpl.php
@@ -8,16 +8,21 @@
 <!DOCTYPE html>
 <html lang="en-US">
   <head>
-    <title>LTW Eats</title>
+    <title>Let's Eat</title>
+    <link rel = "icon" href = "../photos/site/logo.png" type = "image/x-icon">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com"> 
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
+    <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../css/common.css">
     <script src="javascript/script.js" defer></script>
   </head>
   <body>
 
-    <header>
-      <h1><a href="/">LTW Eats</a></h1>
+    <header class="header">
+      <img src="photos/site/logo.png" class="logo">
+      <h1 id="title"><a href="/">Let's Eat</a></h1>
       <?php 
         if ($session->isLoggedIn())
         {
@@ -42,57 +47,64 @@
     </main>
 
     <footer>
-      LTW Eats &copy; 2022
+      <div>
+        <h3 id="foot">Let's Eat &copy; 2022</h3>
+      </div>
     </footer>
   </body>
 </html>
 <?php } ?>
 
 <?php function drawLoginForm() { ?>
-  <form action="../actions/action_login.php" method="post" class="login">
-    <input type="text" name="username" placeholder="username">
-    <input type="password" name="password" placeholder="password">
-    <a href="../pages/register.php">Register</a>
-    <button type="submit">Login</button>
+  <form action="../actions/action_login.php" method="post">
+    <a href="../pages/register.php" class="register">Register</a>
+    <br>
+    <div class="login">
+      <input type="text" name="username" placeholder="username">
+      <input type="password" name="password" placeholder="password">
+      <button type="submit">Login</button>
+    </div>
   </form>
 <?php } ?>
 
 <?php function drawLogoutForm(Session $session) { ?>
   <form action="../actions/action_logout.php" method="post" class="logout">
-    
-    
     <button type="submit">Logout</button>
   </form>
 <?php } ?>
 
 <?php function drawSidebar(Session $session) { ?>
   <div id="mySidebar" class="sidebar">
-  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">×</a>
-  <img class="userphoto" src=<?=$session->getImagePath()?> alt="Profile picture" width=50 height=50>
-  <p><?=$session->getName()?> </p>
-  <a href="../pages/profile.php">Edit Profile</a>
-  <a href="#">Clients</a>
-  <a href="#">Contact</a>
-  <a href="#">&#128722;</a>
-  
-  </div>
+    <nav class="menu">
+      <ul>
+        <li id="user">
+          <img class="userphoto" src=<?=$session->getImagePath()?> alt="Profile picture" width=50 height=50>
+          <p class ="username"><?=$session->getName()?> </p>
+        </li>
+        <li><a href="../pages/profile.php">Edit Profile</a></li>
+        <li><a href="#">Clients</a></li>
+        <li><a href="#">&#128722;</a></li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="javascript:void(0)" class="closebar" onclick="closeNav()">×</a></li>
+      </ul>
+</div>
+</nav>
 
   <div id="main">
-    <button id="buttonsidebar" class="openbtn" onclick="openNav()">☰</button>  
-    
+    <button id="buttonsidebar" class="openbar" onclick="openNav()">☰</button>  
   </div>
 
   <script>
-  function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-    document.getElementById("buttonsidebar").style.visibility = "hidden";
-  }
+    function openNav() {
+      document.getElementById("mySidebar").style.width = "250px";
+      document.getElementById("main").style.marginLeft = "250px";
+      document.getElementById("buttonsidebar").style.visibility = "hidden";
+    }
 
-  function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-    document.getElementById("buttonsidebar").style.visibility = "visible";
-  }
+    function closeNav() {
+      document.getElementById("mySidebar").style.width = "0";
+      document.getElementById("main").style.marginLeft= "0";
+      document.getElementById("buttonsidebar").style.visibility = "visible";
+    }
   </script>
 <?php } ?>
